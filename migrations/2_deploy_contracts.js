edd20e0847deca3c9f289d3af3e81556adb2a2de
Y45Codex <y45codex@protonmail.com>
2021-11-27 17:50:50 +0100

    Add defaults for development deployments

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,12 +4,12 @@ module.exports = async function(deployer, network, accounts) {
     if (network == "live") {
         
     } else {
-        const min_founder_count = 2;
-        const membership_stake = web3.utils.toWei('2', 'ether');
-        const proposal_fee = web3.utils.toWei('0.5', 'ether');
-        const support_fee = web3.utils.toWei('0.25', 'ether');
-        const proposal_polling_time = 3; // 3 seconds
-        const bill_polling_time = 3; // 3 seconds
+        const min_founder_count     = 2;
+        const membership_stake      = web3.utils.toWei('0.1', 'ether');
+        const proposal_fee          = web3.utils.toWei('0.01', 'ether');
+        const support_fee           = web3.utils.toWei('0.001', 'ether');
+        const proposal_polling_time = 120; // seconds
+        const bill_polling_time     = 120; // seconds
 
         await deployer.deploy(
             DAC, 
